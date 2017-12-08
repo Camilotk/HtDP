81ba5b9dde66cfd761e60bea1a9781a62b5d71b1
Camilo Cunha de Azevedo <30880723+Camilotk@users.noreply.github.com>
2017-12-07 22:29:53 -0200

    Add files via upload

---
+++ HtCSP/ex_A04_E01.rkt
@@ -0,0 +1,22 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex_A04_E01) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+"I will dominate Racket"
+(string-append "Inter" " na Série A 2018")
+
+(+ 678 3984)
+" "
+(string-length "Linus told us that is GPL")
+;; string-length retorna sempre a quantidade de
+;; caracteres em uma string
+" "
+(substring "Allonz-y" 0 8)
+(substring "Allonz-y" 3 6)
+"A l l o n z - y"
+"0 1 2 3 4 5 6 7"
+;; 0-based indexing = significa que começamos a
+;; contar a partir de 0 ao invés de 1
+
+;; substring inicia no primeiro valor, mas termina
+;; um valor antes do segundo
+
