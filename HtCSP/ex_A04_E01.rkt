eb6edf1af6806a4947311eeddf1b2f612d41daa7
Camilo Cunha de Azevedo <30880723+Camilotk@users.noreply.github.com>
2017-12-07 22:29:35 -0200

    Delete ex_A04_E01.rkt

--- HtCSP/ex_A04_E01.rkt
+++
@@ -1,5 +0,0 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex_A04_E01) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-"I will dominate Racket"
-(string-append "Ada" " Lovelace")
